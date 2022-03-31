19c7927f166b15cb3985ab4cc8ef01af8a6a9c07
Lagomorph <godzilka@gmail.com>
2022-03-30 21:10:25 -0700

    A list of all pokemon

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [pokemon, setPokemon] = useState([]);
+
+    useEffect(() => {
+        fetch("https:/pokeapi.co/api/v2/pokemon/?limit=1126")
+            .then(response => response.json())
+            .then(response => setPokemon(response.results))
+            .catch((err) => {
+                console.log(err);
+        })
+    }, []);
+
+    return (
+        <div style={{ width: "200px", margin: "auto" }}>
+            {pokemon.length > 0 && pokemon.map((poke, index) => {
+                return (<div key={index}>{poke.name}</div>)
+            })}
+        </div>
+    ); 
 }
 
 export default App;
